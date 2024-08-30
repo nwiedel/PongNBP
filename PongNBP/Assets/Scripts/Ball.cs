4ac9e99a59adae9ad2502a2d75e1a6cd20abd77e
nwiedel <164350832+nwiedel@users.noreply.github.com>
2024-08-30 23:52:49 +0200

    Ball Physik Kein Sound

---
+++ PongNBP/Assets/Scripts/Ball.cs
@@ -0,0 +1,56 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ball : MonoBehaviour
+{
+    public float speed = 30f;
+    public AudioClip ballHitPaddle;
+    public AudioClip ballHitWall;
+    public AudioClip ballMiss;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        GetComponent<Rigidbody2D>().velocity = Vector2.right * speed;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if(collision.gameObject.name == "Spieler1")
+        {
+            // Aufprall mit Spieler
+            float y = HitObject(transform.position, collision.transform.position,
+                collision.collider.bounds.size.y);
+            // Richtung berechnen
+            Vector2 dir = new Vector2(1, y);
+            // Richtungsvector auf die Physik anwenden
+            GetComponent<Rigidbody2D>().velocity = dir * speed;
+            // Audio
+
+        }
+        if (collision.gameObject.name == "Spieler2")
+        {
+            // Aufprall mit Spieler
+            float y = HitObject(transform.position, collision.transform.position,
+                collision.collider.bounds.size.y);
+            // Richtung berechnen
+            Vector2 dir = new Vector2(-1, y);
+            // Richtungsvector auf die Physik anwenden
+            GetComponent<Rigidbody2D>().velocity = dir * speed;
+            // Audio
+            ballHitPaddle.Play();
+        }
+    }
+
+    private float HitObject(Vector2 ballPos, Vector2 schlaegerPos, float schlaegerHoehe)
+    {
+        return (ballPos.y - schlaegerPos.y) / schlaegerHoehe;
+    }
+}
--- PongNBP/Assets/Scripts/Movement.cs
+++ PongNBP/Assets/Scripts/Movement.cs
@@ -5,10 +5,11 @@ using UnityEngine;
 public class Movement : MonoBehaviour
 {
     public float speed = 30f;
+    public string axis;
 
     private void FixedUpdate()
     {
-        float input = Input.GetAxis("Vertical");
+        float input = Input.GetAxis(axis);
         GetComponent<Rigidbody2D>().velocity = 
             new Vector2 (0, input) * speed;
     }
