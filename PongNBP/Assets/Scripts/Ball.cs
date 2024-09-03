d46fa624d0747090c55bad58710088b12cd7a8d8
nwiedel <nwiedel@alte-hof-apotheke.com>
2024-09-03 17:49:18 +0200

    Ende des Kurses

--- PongNBP/Assets/Scripts/Ball.cs
+++ PongNBP/Assets/Scripts/Ball.cs
@@ -1,13 +1,17 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Ball : MonoBehaviour
 {
     public float speed = 30f;
 
-    int scoreSpieler1;
-    int scoreSpieler2;
+    private int scoreSpieler1;
+    private int scoreSpieler2;
+
+    public Text txtSpieler1;
+    public Text txtSpieler2;
 
     public AudioSource audioSource;
     public AudioClip ballHitPaddle;
@@ -18,12 +22,6 @@ public class Ball : MonoBehaviour
     void Start()
     {
         GetComponent<Rigidbody2D>().velocity = Vector2.right * speed;
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
     }
 
     private void OnCollisionEnter2D(Collision2D collision)
@@ -60,11 +58,15 @@ public class Ball : MonoBehaviour
         {
             audioSource.PlayOneShot(ballMiss);
             scoreSpieler1++;
+            txtSpieler1.text = scoreSpieler1.ToString();
+            Restart();
         }
         if (collision.gameObject.name == "WandVertikalRechts")
         {
             audioSource.PlayOneShot(ballMiss);
             scoreSpieler2++;
+            txtSpieler2.text = scoreSpieler2.ToString();
+            Restart();
         }
         Debug.Log(scoreSpieler1 + " : " + scoreSpieler2);
     }
@@ -72,5 +74,10 @@ public class Ball : MonoBehaviour
     private float HitObject(Vector2 ballPos, Vector2 schlaegerPos, float schlaegerHoehe)
     {
         return (ballPos.y - schlaegerPos.y) / schlaegerHoehe;
+    }
+
+    private void Restart()
+    {
+        Vector2 startPosition = new Vector2(2f, 5f);
     }
 }
